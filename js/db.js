e09644a8fc738e993e6dadfad6d1214078d2c95f
FragileClick <fragileclick1@gmail.com>
2026-08-13 21:39:00 -0400

    finished game logic + multi-language + countdown + formatting

--- js/db.js
+++ js/db.js
@@ -1,7 +1,52 @@
-// "img_en": "https://cdn.alteredcore.org/cards/en/CORE/ALT_CORE_B_AX_07_R1.webp",
-// "img_fr": "https://cdn.alteredcore.org/cards/fr/CORE/ALT_CORE_B_AX_07_R1.webp",
-// https://cdn.alteredcore.org/cards/en/CORE/ALT_CORE_B_AX_04_R1.webp
 
+const set_img = [
+    "https://cdn.alteredcore.org/marketing/sets_logo/LOGO BTG-EN.png",
+    "https://cdn.alteredcore.org/marketing/sets_logo/ALT_TBF_LOGO_en_EN.png",
+    "https://cdn.alteredcore.org/marketing/sets_logo/ALT_WFM_LOGO_en_EN.png",
+    "https://cdn.alteredcore.org/marketing/sets_logo/ALT_SKY_LOGO_en_EN.png",
+    "https://cdn.alteredcore.org/marketing/sets_logo/ALT_SDU_LOGO_EN.png",
+    "https://cdn.alteredcore.org/marketing/sets_logo/ALT_ROC_LOGO_en_EN.png"
+]
+
+const faction_img = [
+    "https://cdn.alteredcore.org/marketing/game_assets/faction_banners/AXIOM-faction-banner.png",
+    "https://cdn.alteredcore.org/marketing/game_assets/faction_banners/BRAVOS-faction-banner.png",
+    "https://cdn.alteredcore.org/marketing/game_assets/faction_banners/LYRA-faction-banner.png",
+    "https://cdn.alteredcore.org/marketing/game_assets/faction_banners/MUNA-faction-banner.png",
+    "https://cdn.alteredcore.org/marketing/game_assets/faction_banners/ORDIS-faction-banner.png",
+    "https://cdn.alteredcore.org/marketing/game_assets/faction_banners/YZMIR-faction-banner.png"
+]
+
+const text = {
+    "en": {
+        "hero_text": "Card guessing game for ",
+        "input_hint": "Guess a card...",
+        "game_board_header_set": "SET",
+        "game_board_header_faction": "FACTION",
+        "game_board_header_type": "TYPE",
+        "game_board_header_subtype": "SUBTYPE",
+        "countdown_title": "Next puzzle in:",
+        "result_title_success": "Well done!",
+        "result_subtitle_success": "You guessed <b>CARD_NAME</b> in <b>ATTEMPTS</b> tries.",
+        "result_title_failure": "So close!",
+        "result_subtitle_failure": "The card was <b>CARD_NAME</b>.",
+        "footer_attribution_article": "by"
+    },
+    "fr": {
+        "hero_text": "Jeu de devinettes de cartes pour ",
+        "input_hint": "Devinez une carte...",
+        "game_board_header_set": "SET",
+        "game_board_header_faction": "FACTION",
+        "game_board_header_type": "TYPE",
+        "game_board_header_subtype": " SOUS-TYPE",
+        "countdown_title": "Prochain casse-tête:",
+        "result_title_success": "Bien joué!",
+        "result_subtitle_success": "Vous avez deviné <b>CARD_NAME</b> en <b>ATTEMPTS</b> essais.",
+        "result_title_failure": "Presque!",
+        "result_subtitle_failure": "La carte était <b>CARD_NAME</b>.",
+        "footer_attribution_article": "de"
+    }
+}
 
 const cards = [
     {
@@ -10,8 +55,12 @@ const cards = [
         "name_fr": "Mécanicienne de la Fonderie",
         "set": 1,
         "faction": 1,
-        "type": "C",
-        "subtype": "Engineer"
+        "type_en": "Character",
+        "type_fr": "Personnage",
+        "subtype_en": "Engineer",
+        "subtype_fr": "Ingénieur",
+        "img_en": "https://cdn.alteredcore.org/cards/en/CORE/ALT_CORE_B_AX_07_R1.webp",
+        "img_fr": "https://cdn.alteredcore.org/cards/fr/CORE/ALT_CORE_B_AX_07_R1.webp"
     },
     {
         "id": "ALT_CORE_B_AX_04_R1",
@@ -19,45 +68,24 @@ const cards = [
         "name_fr": "Élémentaire de Kélon",
         "set": 1,
         "faction": 1,
-        "type": "C",
-        "subtype": "Elemental"
+        "type_en": "Character",
+        "type_fr": "Personnage",
+        "subtype_en": "Elemental",
+        "subtype_fr": "Élémentaire",
+        "img_en": "https://cdn.alteredcore.org/cards/en/CORE/ALT_CORE_B_AX_04_R1.webp",
+        "img_fr": "https://cdn.alteredcore.org/cards/fr/CORE/ALT_CORE_B_AX_04_R1.webp"
     },
     {
         "id": "ALT_CORE_B_BR_04_R1",
         "name_en": "Ratatoskr",
+        "img_en": "https://cdn.alteredcore.org/cards/en/CORE/ALT_CORE_B_BR_04_R1.webp",
+        "img_fr": "https://cdn.alteredcore.org/cards/fr/CORE/ALT_CORE_B_BR_04_R1.webp",
+        "type_en": "Character",
+        "type_fr": "Personnage",
+        "subtype_en": "Messenger",
+        "subtype_fr": "Émissaire",
         "name_fr": "Ratatosk",
         "set": 1,
         "faction": 2,
-        "type": "C",
-        "subtype": "Messenger"
     }
 ]
-
-
-// cards = {
-//     "ALT_CORE_B_AX_07_R1": {
-//         "name_en": "Foundry Mechanic",
-//         "name_fr": "Mécanicienne de la Fonderie",
-//         "set": 1,
-//         "faction": 1,
-//         "type": "C",
-//         "subtype": "Engineer"
-//     },
-//     "ALT_CORE_B_AX_04_R1": {
-//         "name_en": "Kelon Elemental",
-//         "name_fr": "Élémentaire de Kélon",
-//         "set": 1,
-//         "faction": 1,
-//         "type": "C",
-//         "subtype": "Elemental"
-//     },
-//     "ALT_CORE_B_BR_04_R1": {
-//         "name_en": "Ratatoskr",
-//         "name_fr": "Ratatosk",
-//         "set": 1,
-//         "faction": 2,
-//         "type": "C",
-//         "subtype": "Messenger"
-//     }
-// }
-
--- js/main.js
+++ js/main.js
@@ -1,72 +1,135 @@
-// import Fuse from './fuse.js'
-
-
-// SELECTORS
+// GAME SELECTORS
 const game_search_input        = document.getElementById('game_search_input')
 const game_search_autocomplete = document.getElementById('game_search_autocomplete')
+const game_result              = document.getElementById('game_result')
+const game_result_img          = document.getElementById('game_result_img')
+const countdown                = document.getElementById('countdown')
+// TEXT SELECTOR
+const hero_text = document.getElementById('hero_text') 
+const input_hint = document.getElementById('input_hint') 
+const game_board_header_set = document.getElementById('game_board_header_set') 
+const game_board_header_faction = document.getElementById('game_board_header_faction') 
+const game_board_header_type = document.getElementById('game_board_header_type') 
+const game_board_header_subtype = document.getElementById('game_board_header_subtype') 
+const countdown_title = document.getElementById('countdown_title') 
+const result_title_success = document.getElementById('result_title_success') 
+const result_subtitle_success = document.getElementById('result_subtitle_success') 
+const result_title_failure = document.getElementById('result_title_failure') 
+const result_subtitle_failure = document.getElementById('result_subtitle_failure')
+const footer_attribution_article = document.getElementById('footer_attribution_article')
+const language_toggle_fr = document.getElementById('language_toggle_fr')
+const language_toggle_en = document.getElementById('language_toggle_en')
+// GAME VARIABLES
+var TARGET_CARD          = cards[0]
+var PLAYER_GUESS_CURRENT = 0
+var PLAYER_GUESS_TOTAL   = 6
+var LANGUAGE             = 'en'
+// DATABASE SEARCH INDEX
+const fuse_en = new Fuse(cards, {keys: ['name_en']})
+const fuse_fr = new Fuse(cards, {keys: ['name_fr']})
 
-var TARGET_CARD     = 'ALT_CORE_B_BR_04_R1'
-var PLAYER_ATTEMPTS = 0
-var TOTAL_ATTEMPTS  = 6
-var LANGUAGE        = 'EN'
+function setLanguage() {
+    // Update game text
+    var copy = text[LANGUAGE]
+    hero_text.innerText = copy.hero_text
+    game_search_input.placeholder = copy.input_hint
+    game_board_header_set.innerText = copy.game_board_header_set
+    game_board_header_faction.innerText = copy.game_board_header_faction
+    game_board_header_type.innerText = copy.game_board_header_type
+    game_board_header_subtype.innerText = copy.game_board_header_subtype
+    countdown_title.innerText = copy.countdown_title
+    result_title_success.innerText = copy.result_title_success
+    result_subtitle_success.innerHTML = copy.result_subtitle_success.replace('CARD_NAME', TARGET_CARD.name_en).replace('ATTEMPTS', PLAYER_GUESS_CURRENT)
+    result_title_failure.innerText = copy.result_title_failure
+    result_subtitle_failure.innerHTML = copy.result_subtitle_failure.replace('CARD_NAME', TARGET_CARD.name_en)
+    footer_attribution_article.innerText = copy.footer_attribution_article
 
-const fuse = new Fuse(cards, {keys: ['name_en']})
+    // Update language toggle icon
+    if (LANGUAGE=='fr') {
+        language_toggle_en.classList.add('translucent')
+        language_toggle_fr.classList.remove('translucent')
+    } else {
+        language_toggle_en.classList.remove('translucent')
+        language_toggle_fr.classList.add('translucent')
+    }
+}
+
+function toggleLanguage() {
+    if (LANGUAGE == 'fr') {
+        LANGUAGE = 'en'
+    } else {
+        LANGUAGE = 'fr'
+    }
+    setLanguage()
+}
 
 function player_search(self) {
 
+    // Get player search query string
     var player_input = game_search_input.value
 
-    // Search for results based on player input
-    var search_results = fuse.search(player_input)
-
+    // If player hasn't searched anything, hide menu
     if (player_input.length == 0) {
         game_search_autocomplete.classList.add('hidden')
         return
     }
 
-    // Empty autocomplete menu
+    // Search cardlist
+    if (LANGUAGE=='fr'){
+        var search_results = fuse_fr.search(player_input)
+    } else {
+        var search_results = fuse_en.search(player_input)
+    }
+
+    // Build menu with search results
+    // First empty the previous list
     game_search_autocomplete.innerHTML = ""
 
-    if (search_results.length > 0) {
-
-        // If the query returns search results
+    if (search_results.length <= 0) {
+        // If no search results match the query
+        // append an item saying nothing was found
+        var li = document.createElement("li")
+        li.textContent = "No results found."
+        game_search_autocomplete.appendChild(li)
+    } else {
+        // If the query does return search results
         // add result items to the autocomplete menu
         for (let i = 0; i < search_results.length; i++) {
 
-            const card = search_results[i].item
-            const li = document.createElement("li")
-            const text = document.createElement("span")
-            const img = document.createElement("img")
+            // Build the list item
+            var card = search_results[i].item
+            var li = document.createElement("li")
+            var text = document.createElement("span")
+            var img = document.createElement("img")
 
             // Make first li "selected"
             if (i==0) {
                 li.classList.add('selected')
             }
 
-            li.id = card.id
+            // Set list itme properties
+            if (LANGUAGE == 'fr'){
+                text.textContent = card.name_fr
+                img.src = card.img_fr
+            } else {
+                text.textContent = card.name_en
+                img.src = card.img_en
+            }
             li.card = card
-            text.textContent = card.name_en
-            img.src = "https://cdn.alteredcore.org/cards/en/CORE/"+card.id+".webp"
+            text.card = card
+            img.card = card
 
+            // Write list item to 
             li.appendChild(img)
             li.appendChild(text)
-
             game_search_autocomplete.appendChild(li)
 
+            // Show the the top 10 results, at most
             if (i>10) {
                 break
             }
-            
         }
-
-    } else {
-        // If no search results match the query
-        // append an item saying nothing was found
-        const li = document.createElement("li")
-        li.textContent = "No results found."
-        game_search_autocomplete.appendChild(li)
     }
-
     // Reveal autocomplete menu showing results
     game_search_autocomplete.classList.remove('hidden')
 }
@@ -85,6 +148,108 @@ function player_guess(card) {
     game_search_input.value = ""
     game_search_autocomplete.classList.add('hidden')
 
-    console.log(card)
+    PLAYER_GUESS_CURRENT += 1
 
+    // Select the game board elements that need to update
+    var tile_set     = document.getElementById('game_tile_set_'+PLAYER_GUESS_CURRENT)
+    var tile_faction = document.getElementById('game_tile_faction_'+PLAYER_GUESS_CURRENT)
+    var tile_type    = document.getElementById('game_tile_type_'+PLAYER_GUESS_CURRENT)
+    var tile_subtype = document.getElementById('game_tile_subtype_'+PLAYER_GUESS_CURRENT)
+
+    var guess_card    = document.getElementById('game_guess_card_'+PLAYER_GUESS_CURRENT)
+    var guess_set     = document.getElementById('game_guess_set_'+PLAYER_GUESS_CURRENT)
+    var guess_faction = document.getElementById('game_guess_faction_'+PLAYER_GUESS_CURRENT)
+    var guess_type    = document.getElementById('game_guess_type_'+PLAYER_GUESS_CURRENT)
+    var guess_subtype = document.getElementById('game_guess_subtype_'+PLAYER_GUESS_CURRENT)
+
+    // Write guess card attributes to gameboard
+    if (LANGUAGE=='fr') {
+        guess_card.src = card.img_fr
+        guess_set.src = set_img[card.set-1]
+        guess_faction.src = faction_img[card.faction-1]
+        guess_type.innerText = card.type_fr
+        guess_subtype.innerText = card.subtype_fr
+    } else {
+        guess_card.src = card.img_en
+        guess_set.src = set_img[card.set-1]
+        guess_faction.src = faction_img[card.faction-1]
+        guess_type.innerText = card.type_en
+        guess_subtype.innerText = card.subtype_en
+    }
+
+    // Color tiles true or false
+    if (card.set == TARGET_CARD.set) {
+        tile_set.classList.add('true')
+    } else {
+        tile_set.classList.add('false')
+    }
+    if (card.faction == TARGET_CARD.faction) {
+        tile_faction.classList.add('true')
+    } else {
+        tile_faction.classList.add('false')
+    }
+    if (card.type_en == TARGET_CARD.type_en) {
+        tile_type.classList.add('true')
+    } else {
+        tile_type.classList.add('false')
+    }
+    if (card.subtype_en == TARGET_CARD.subtype_en) {
+        tile_subtype.classList.add('true')
+    } else {
+        tile_subtype.classList.add('false')
+    }
+
+    // If player guessed the correct card
+    // the game is over
+    if (card == TARGET_CARD) {
+        // Hide input and show result
+        game_search_input.classList.add('hidden')
+        countdown.classList.remove('hidden')
+        game_result_success.classList.remove('hidden')
+
+        // Add image
+        if (LANGUAGE == 'fr') {
+            game_result_img_success.src = TARGET_CARD.img_fr
+        } else {
+            game_result_img_success.src = TARGET_CARD.img_en
+        }
+
+        // Redraw text
+        setLanguage()
+
+        // Scroll to bottom
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    // If player ran out of guess attempts
+    // the game is over
+    if (PLAYER_GUESS_CURRENT >= PLAYER_GUESS_TOTAL) {
+        // Hide input and show result
+        game_search_input.classList.add('hidden')
+        countdown.classList.remove('hidden')
+        game_result_failure.classList.remove('hidden')
+        
+        // Add image
+        if (LANGUAGE == 'fr') {
+            game_result_img_failure.src = TARGET_CARD.img_fr
+        } else {
+            game_result_img_failure.src = TARGET_CARD.img_en
+        }
+
+        // Redraw text
+        setLanguage()
+
+        // Scroll to bottom
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
 }
+
+setLanguage()
